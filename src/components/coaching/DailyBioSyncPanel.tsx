3a0c8025fae96868792af6f477ad638903aab7cc
Master H <masterh@myeongsim.com>
2026-05-13 08:12:03 +0900

    feat: 아카식 레코드 일진(십성) 기반 동적 연동 고도화 완료

--- src/components/coaching/DailyBioSyncPanel.tsx
+++ src/components/coaching/DailyBioSyncPanel.tsx
@@ -909,7 +909,7 @@ export default function DailyBioSyncPanel() {
                     initial={{ opacity: 0, x: -10 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: 10 }}
                     transition={{ duration: 0.25 }}
                   >
-                    <AkashicRecordSection dayMasterHanja={dayMasterHanja} sajuData={reportData} />
+                    <AkashicRecordSection dayMasterHanja={dayMasterHanja} sajuData={reportData} harmony={harmony} />
                   </motion.div>
                 )}
 
